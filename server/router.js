91293945ecbde74a3cf21d57852466168d8b3204
NLukyanov <nikolay.lukyanov@dataart.com>
2015-05-30 20:23:56 +0300

    Updated router.js for auto-deploy

--- server/router.js
+++ server/router.js
@@ -1,3 +1,4 @@
+var exec = require('child_process').exec;
 function router(app) {
 	app.get('/site', function(req, resp) {
 		var Site = require('./site/site');
@@ -5,6 +6,11 @@ function router(app) {
 		site = new Site(req, resp);
 
 		site.renderSite();
+	});
+	app.get('/push', function(req, resp) {
+		var child = exec('git pull', function(err, stdout, stderr) {
+			resp.end();
+		});
 	});
 }
 module.exports = router;
