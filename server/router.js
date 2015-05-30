1ef1063866f4a6f3a22dd3ac1ab34fe9be37b892
NLukyanov <nikolay.lukyanov@dataart.com>
2015-05-31 00:19:30 +0300

    Changed homepage path

--- server/router.js
+++ server/router.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 function router(app) {
-	app.get('/site', function(req, resp) {
+	app.get('/', function(req, resp) {
 		var Site = require('./site/site');
 
 		site = new Site(req, resp);
