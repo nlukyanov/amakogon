ce840fe45482790aa73cc8a0b0b8ce2e13876398
NLukyanov <nikolay.lukyanov@dataart.com>
2015-06-04 21:17:24 +0300

    Updated styles

--- client/site/js/app/components/siteAlbum.js
+++ client/site/js/app/components/siteAlbum.js
@@ -25,12 +25,14 @@
 			$('.flex-next').html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 561" version="1.1"><g id="#ffffffff"></g><g id="#242424ff"><path opacity="1.00" d=" M 45.54 0.00 L 53.46 0.00 C 63.21 0.81 72.60 4.21 80.99 9.17 C 205.79 81.31 330.61 153.43 455.42 225.56 C 463.12 230.20 471.27 234.17 478.45 239.65 C 490.05 248.47 498.89 261.72 500.00 276.52 L 500.00 283.63 C 498.55 303.05 484.21 318.89 467.65 327.73 C 343.65 399.35 219.67 470.99 95.68 542.61 C 84.42 548.81 73.69 556.65 60.90 559.39 C 46.41 562.89 29.97 560.27 18.52 550.32 C 6.13 540.05 0.60 523.67 0.00 507.99 L 0.00 51.77 C 0.75 39.26 4.57 26.57 12.82 16.92 C 20.81 7.09 32.96 1.00 45.54 0.00 M 47.97 45.59 C 46.37 48.81 46.09 52.44 46.12 55.97 C 46.11 205.30 46.12 354.63 46.12 503.96 C 46.14 507.67 46.21 511.58 48.05 514.92 C 51.33 514.29 54.50 513.13 57.35 511.37 C 186.01 437.01 314.67 362.63 443.34 288.26 C 447.26 286.00 451.55 283.88 454.11 279.97 C 450.73 275.77 445.73 273.55 441.22 270.85 C 313.22 196.84 185.21 122.83 57.21 48.82 C 54.36 47.20 51.27 45.88 47.97 45.59 Z"/></g></svg>')
 
 			scope.imgFullsize = function() {
-				element.toggleClass('fullsize').css({visibility: 'hidden'});
-				setTimeout(function() {
-					$(window).trigger('resize');
-					element.css({visibility: 'visible'});
-				}, 100);
-				$('.logo, .toggleNav, .breadcrumbs').toggleClass('fixedAlbum');
+				if ( !element.hasClass('fullsize') ) {
+					element.addClass('fullsize').css({visibility: 'hidden'});
+					setTimeout(function() {
+						$(window).trigger('resize');
+						element.css({visibility: 'visible'});
+					}, 100);
+					$('.logo, .toggleNav, .breadcrumbs').addClass('fixedAlbum');
+				}
 			}
 
 			scope.closeFullsize = function(e) {
